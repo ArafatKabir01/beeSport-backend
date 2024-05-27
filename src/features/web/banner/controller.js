1c1eeac115e8aa7adb5e61fe5dde8d7907b19e52
ArafatKabir01 <nabil786.ak@gmail.com>
2024-05-27 17:06:43 +0600

    fixed bug

--- src/features/web/banner/controller.js
+++ src/features/web/banner/controller.js
@@ -1,32 +1,30 @@
-const Banner = require("../../admin/banner/model");
-
-
+const Banner = require("../../admin/Banner/model");
 
 exports.getAllBanners = async (req, res, next) => {
-    try {
-      const { page = 1, limit = 10 } = req.query;
-      const skip = (page - 1) * limit;
-  
-      const [docs, total] = await Promise.all([
-        Banner.find({}).limit(limit).skip(skip).sort({ publishDate: "desc" }),
-        Banner.countDocuments()
-      ]);
-  
-      const hasNext = total > skip + limit;
-      const hasPrev = page > 1;
-  
-      return res.json({
-        status: true,
-        message: "Banner fetched successfully!",
-        data: docs,
-        page: +page,
-        limit: +limit,
-        totalPage: Math.ceil(total / limit),
-        totalDocs: total,
-        hasNext,
-        hasPrev
-      });
-    } catch (error) {
-      next(error);
-    }
+  try {
+    const { page = 1, limit = 10 } = req.query;
+    const skip = (page - 1) * limit;
+
+    const [docs, total] = await Promise.all([
+      Banner.find({}).limit(limit).skip(skip).sort({ publishDate: "desc" }),
+      Banner.countDocuments()
+    ]);
+
+    const hasNext = total > skip + limit;
+    const hasPrev = page > 1;
+
+    return res.json({
+      status: true,
+      message: "Banner fetched successfully!",
+      data: docs,
+      page: +page,
+      limit: +limit,
+      totalPage: Math.ceil(total / limit),
+      totalDocs: total,
+      hasNext,
+      hasPrev
+    });
+  } catch (error) {
+    next(error);
   }
+};
